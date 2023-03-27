1ae267b36c38e9949d0b65798b256ca1bb121f12
Gerard Darris <darrisgr@gmail.com>
2023-03-27 12:53:56 -0500

    Finished Views

--- AnimalRoster6/Controllers/AnimalsController.cs
+++ AnimalRoster6/Controllers/AnimalsController.cs
@@ -10,10 +10,37 @@ namespace AnimalRoster6.Controllers
 {
     public class AnimalsController : Controller
     {
+        private static List<Dictionary<string, string>> Animals = new List<Dictionary<string, string>>();
+
+        [HttpGet]
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.animals = Animals;
             return View();
+        }
+
+        [HttpGet]
+        [Route("Animals/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("Animals/Add")]
+        public IActionResult Add(string name, string species, string desc, string imgUrl)
+        {
+            Animals.Add(new Dictionary<string, string>
+            {
+                {"name", name },
+                {"species", species },
+                {"description", desc },
+                {"imgUrl", imgUrl }
+            });
+
+            return Redirect("/Animals");
         }
     }
 }
