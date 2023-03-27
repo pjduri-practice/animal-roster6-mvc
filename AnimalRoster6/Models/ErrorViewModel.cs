be6b62c027533966d77cd4621b613359bbd308e4
Gerard Darris <darrisgr@gmail.com>
2023-03-27 12:10:04 -0500

    Initial commit

---
+++ AnimalRoster6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimalRoster6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
